f6484f2dce354d5e0fa486171269b97d7459df1d
root <henriquiui70@gmail.com>
2017-11-18 18:08:46 -0200

    arq mainlista

---
+++ Lista/mainlista.c
@@ -0,0 +1,18 @@
+#include "lista.h"
+#include <stdio.h>
+int main (void){
+    cList* list = list_create();
+    add_cNode(0, 10, list);
+    add_cNode(1, 21, list);
+    add_cNode(2, -91, list);
+    printList(list);
+    printf("----------------------\n");
+    rem_cNode(1, list);
+    add_cNode(0, 33, list);
+    printList(list);
+
+
+
+    free_list(list);
+    return 0;
+}
