a4b8a40c368b410ca8bcc8fa54f9e0c535dea019
oszoto <33788856+oszoto@users.noreply.github.com>
2017-11-20 17:15:42 -0200

    implementada funcao tree_fill

--- Arvore/mainarvore.c
+++ Arvore/mainarvore.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 
 int main(void){
+
     t_node* node = node_create();
+    tree_fill( node );
+    tree_fill( node );
 
-
-
-
-
-
+    printf("%x", node->right->right); /* pa comprova */
 
     return 0;
 }
