b35a0cf0c701563df380355e73ff88adb130683d
root <henriquiui70@gmail.com>
2017-11-28 15:16:12 -0200

    função escolha com aleatoriedade bugada

--- Arvore_com_lista/escolha.c
+++ Arvore_com_lista/escolha.c
@@ -3,11 +3,34 @@
 void escolha(cList* list){
     cNode* element = NULL;
     element = list->first;
-
+    srand(time(NULL));
     for(int i = 0; i < NUM; i++){
 
         printf("Personagem %d:\n", (i + 1));
-        printf("Agility: %d  Strength: %d  Intelligence: %d  Health: %d\n", element->character->agility, element->character->strength, element->character->intelligence, element->character->health);
+
+        if(rand() % 4 == 0){
+            printf("Agility: %d  ", element->character->agility);
+        } else {
+            printf("Agility: **  ");
+        }
+
+        if(rand() % 4 == 1){
+            printf("Strength: %d  ", element->character->strength);
+        } else {
+            printf("Strength: **  ");
+        }
+
+        if(rand() % 4 == 2){
+            printf("Intelligence: %d  ", element->character->intelligence);
+        } else {
+            printf("Intelligence: **  ");
+        }
+
+        if(rand() % 4 == 3){
+            printf("Health: %d\n", element->character->health);
+        } else {
+            printf("Health: **\n");
+        }
         element = element->next;
 
     }/*end for()*/
