96e3112abca54933e551c3b97ec36b612f8db518
oszoto <33788856+oszoto@users.noreply.github.com>
2017-11-28 17:59:51 -0200

    correção do codigo, nao testado

--- Arvore_com_lista/escolha.c
+++ Arvore_com_lista/escolha.c
@@ -4,29 +4,30 @@ void escolha(cList* list){
     cNode* element = NULL;
     element = list->first;
     srand(time(NULL));
+    int choice = srand();
     for(int i = 0; i < NUM; i++){
 
         printf("Personagem %d:\n", (i + 1));
 
-        if(rand() % 4 == 0){
+        if(choice % 4 == 0){
             printf("Agility: %d  ", element->character->agility);
         } else {
             printf("Agility: **  ");
         }
 
-        if(rand() % 4 == 1){
+        if(choice % 4 == 1){
             printf("Strength: %d  ", element->character->strength);
         } else {
             printf("Strength: **  ");
         }
 
-        if(rand() % 4 == 2){
+        if(choice % 4 == 2){
             printf("Intelligence: %d  ", element->character->intelligence);
         } else {
             printf("Intelligence: **  ");
         }
 
-        if(rand() % 4 == 3){
+        if(choice % 4 == 3){
             printf("Health: %d\n", element->character->health);
         } else {
             printf("Health: **\n");
