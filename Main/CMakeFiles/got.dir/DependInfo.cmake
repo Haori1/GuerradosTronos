d8ba01ce5e62dee3621f49a4e0496bca99872fbc
root <henriquiui70@gmail.com>
2017-12-03 14:19:54 -0200

    Makefile topzera

---
+++ Main/CMakeFiles/got.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/root/GuerradosTronos/Main/arvore.c" "/root/GuerradosTronos/Main/CMakeFiles/got.dir/arvore.c.o"
+  "/root/GuerradosTronos/Main/batalha.c" "/root/GuerradosTronos/Main/CMakeFiles/got.dir/batalha.c.o"
+  "/root/GuerradosTronos/Main/escolha.c" "/root/GuerradosTronos/Main/CMakeFiles/got.dir/escolha.c.o"
+  "/root/GuerradosTronos/Main/fila.c" "/root/GuerradosTronos/Main/CMakeFiles/got.dir/fila.c.o"
+  "/root/GuerradosTronos/Main/game.c" "/root/GuerradosTronos/Main/CMakeFiles/got.dir/game.c.o"
+  "/root/GuerradosTronos/Main/lertxt.c" "/root/GuerradosTronos/Main/CMakeFiles/got.dir/lertxt.c.o"
+  "/root/GuerradosTronos/Main/lista.c" "/root/GuerradosTronos/Main/CMakeFiles/got.dir/lista.c.o"
+  "/root/GuerradosTronos/Main/main.c" "/root/GuerradosTronos/Main/CMakeFiles/got.dir/main.c.o"
+  "/root/GuerradosTronos/Main/tela_inicial.c" "/root/GuerradosTronos/Main/CMakeFiles/got.dir/tela_inicial.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
